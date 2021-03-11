c857514925883a818d6c713f9fbd78f04f452494
Chris <cthompson98@bcit.ca>
2021-03-11 10:46:01 -0800

    Hello world

---
+++ README.md
@@ -0,0 +1,1 @@
+### COMP 1510 Merge demo
---
+++ merge.py
@@ -0,0 +1,17 @@
+"""
+Comment.
+"""
+
+
+def factorial(upper_bound):
+    pass
+
+
+def main():
+    """
+    Drive the program.
+    """
+
+
+if __name__ == "__main__":
+    main()
